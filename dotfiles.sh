577c1da257d0db8a83843cd691d31c9de904d92b
samihda <samihda@users.noreply.github.com>
2018-03-27 01:05:30 +0200

    Fix init script

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
 git clone --separate-git-dir=$HOME/.dotfiles https://github.com/samihda/dotfiles.git .dotfiles-tmp
-rsync -rv --exclude '.git' .dotfiles-tmp/ $HOME/
-rm -r .dotfiles-tmp
+
+rm -rf $HOME/.dotfiles-tmp/.git
+cp -R $HOME/.dotfiles-tmp/. $HOME/
+rm -rf $HOME/.dotfiles-tmp/
 
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 dotfiles config status.showUntrackedFiles no
