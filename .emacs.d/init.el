f3917d9617c0640780d18158f094abcda221654f
samihda <samihda@users.noreply.github.com>
2025-11-18 12:07:26 +0100

    Fix indentation

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -51,8 +51,8 @@
   :commands project-find-functions
   :config
   (defun project-find-go-module (dir)
-  (when-let ((root (locate-dominating-file dir "go.mod")))
-    (cons 'go-module root)))
+    (when-let ((root (locate-dominating-file dir "go.mod")))
+      (cons 'go-module root)))
   (cl-defmethod project-root ((project (head go-module)))
     (cdr project))
   (add-hook 'project-find-functions #'project-find-go-module))
